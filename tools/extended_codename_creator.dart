e7b585416c8e7e390680746c8b37cdf525ee8f32
PiotrZPL <zpl20052020@protonmail.ch>
2023-01-26 13:11:46 +0100

    Updated ExtendedCodenameCreator for ArrowOS

--- tools/extended_codename_creator.dart
+++ tools/extended_codename_creator.dart
@@ -8,10 +8,22 @@ String extendedCodenameCreator({
   else if (readCodename == "apollon" && vendor == "Xiaomi") {
     return "xiaomi-apollo";
   }
-  else if (readCodename == "merlinx" && vendor == "Xiaomi") {
+  else if (readCodename == "mojito" && vendor == "Redmi") {
+    return "xiaomi-mojito";
+  }
+  else if (readCodename == "picasso" && vendor == "Redmi") {
+    return "xiaomi-picasso";
+  }
+  else if (readCodename == "sweet" && vendor == "Redmi") {
+    return "xiaomi-sweet";
+  }
+  else if (readCodename == "merlinx" && (vendor == "Xiaomi" || vendor == "Redmi")) {
     return "xiaomi-merlin";
   }
-  else if (readCodename == "phoenix" && vendor == "Redmi") {
+  else if (readCodename == "lmi" && (vendor == "Redmi" || vendor == "POCO" || vendor == "Poco")) {
+    return "xiaomi-lmi";
+  }
+  else if (readCodename == "phoenix" && (vendor == "Redmi" || vendor == "POCO" || vendor == "Poco")) {
     return "xiaomi-phoenix";
   }
   else if (readCodename == "alioth" && (vendor == "Poco" || vendor == "POCO")) {
@@ -19,6 +31,12 @@ String extendedCodenameCreator({
   }
   else if (readCodename == "vayu" && (vendor == "Poco" || vendor == "POCO")) {
     return "xiaomi-vayu";
+  }
+  else if (readCodename == "veux" && (vendor == "Poco" || vendor == "POCO")) {
+    return "xiaomi-veux";
+  }
+  else if (readCodename == "munch" && (vendor == "Poco" || vendor == "POCO")) {
+    return "xiaomi-munch";
   }
 
   // See xiaomi-surya description
@@ -33,6 +51,10 @@ String extendedCodenameCreator({
   }
   else if (readCodename == "z2_plus" && vendor == "Lenovo") {
     return "zuk-z2_plus";
+  }
+
+  else if (readCodename == "G" && vendor == "10or") {
+    return "10.or-G";
   }
   else {
     return "${vendor.toLowerCase()}-$readCodename";
