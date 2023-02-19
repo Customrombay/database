4b67cc662664a7f35029a42a0a1160d74474987b
PiotrZPL <zpl20052020@protonmail.ch>
2023-02-19 14:30:32 +0100

    Added support for syncing with PixelOS

---
+++ syncWithPixelOS.dart
@@ -0,0 +1,105 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:yaml/yaml.dart';
+import 'package:yaml_writer/yaml_writer.dart';
+import 'tools/extended_codename_creator.dart';
+import 'tools/android_version_from_evolutionx_version.dart';
+
+void main() async {
+  int numberOfCovered = 0;
+  int numberOfNotCovered = 0;
+  List<String> listOfNotCovered = [];
+  var response = await http.get(Uri.parse("https://raw.githubusercontent.com/PixelOS-AOSP/official_devices/thirteen/API/devices.json"));
+  if (response.statusCode == 200) {
+    stdout.write("OK\n");
+    YamlMap ydoc = loadYaml(response.body);
+    YamlList listOfDevices = ydoc["devices"];
+    for (YamlMap device in listOfDevices) {
+      String readCodename = device["codename"];
+      String readVendor = device["vendor"];
+      String extendedCodename = extendedCodenameCreator(readCodename: readCodename, vendor: readVendor);
+      if (isSupported(extendedCodename: extendedCodename)) {
+        numberOfCovered += 1;
+        addToSupport(
+          androidVersion: "13",
+          extendedCodename: extendedCodename,
+          romName: "PixelOS",
+          romState: "Official",
+          romSupport: true,
+          romWebpage: "https://pixelos.net/",
+          deviceWebpage: "https://pixelos.net/download/$readCodename"
+        );
+      }
+      else {
+        numberOfNotCovered += 1;
+        listOfNotCovered += [extendedCodename];
+      }
+    }
+    
+    stdout.write("Covered: $numberOfCovered\n");
+    stdout.write("Not covered: $numberOfNotCovered\n");
+    for (var deviceNotCovered in listOfNotCovered) {
+      stdout.write("$deviceNotCovered\n");
+    }
+  }
+}
+
+bool isSupported({
+  required String extendedCodename
+}) {
+  if (File("database/phone_data/$extendedCodename.yaml").existsSync()) {
+    return true;
+  }
+  return false;
+}
+
+void addToSupport({String androidVersion = "", String extendedCodename = "", String romName = "", String romState = "", bool romSupport = false, String romWebpage = "", String deviceWebpage = ""}) async {
+  File deviceFile = File("database/phone_data/$extendedCodename.yaml");
+  String thisFileContent = await deviceFile.readAsString();
+  var thisFileyaml = loadYaml(thisFileContent);
+  List newList = [];
+  bool alreadySupported = false;
+  for (var thisRom in thisFileyaml["roms"]) {
+    String thisRomName = thisRom["rom-name"];
+    if (thisRomName == romName) {
+      alreadySupported = true;
+      newList += [
+        {
+          "rom-name": romName,
+          "rom-support": romSupport,
+          "rom-state": romState,
+          "android-version": androidVersion,
+          "rom-webpage": romWebpage,
+          "phone-webpage": deviceWebpage
+        }
+      ];
+    }
+    else {
+      newList += [thisRom];
+    }
+  }
+
+  if (!alreadySupported) {
+    newList += <dynamic>[
+      {
+        "rom-name": romName,
+        "rom-support": romSupport,
+        "rom-state": romState,
+        "android-version": androidVersion,
+        "rom-webpage": romWebpage,
+        "phone-webpage": deviceWebpage
+      }
+    ];
+  }
+
+  Map newMap = {
+    "device-name" : thisFileyaml["device-name"],
+    "device-vendor": thisFileyaml["device-vendor"],
+    "device-model-name": thisFileyaml["device-model-name"],
+    "device-description": thisFileyaml["device-description"],
+    "roms": newList
+  };
+
+  // File newFile = File("newfiles/${vendor.toString().toLowerCase()}-$codename.yaml");
+  await deviceFile.writeAsString(YAMLWriter().write(newMap));
+}
