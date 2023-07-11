337ab25c62aa8a10c79b71b6a754e6b577219071
PiotrZPL <zpl20052020@protonmail.ch>
2023-07-11 23:09:59 +0200

    Added support for syncing with CherishOS

---
+++ sync_with_cherish_os.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:yaml/yaml.dart';
+import 'tools/extended_codename_creator.dart';
+import 'tools/is_supported.dart';
+import 'tools/add_to_support.dart';
+
+void main() async {
+  int numberOfCovered = 0;
+  int numberOfNotCovered = 0;
+  List<String> listOfNotCovered = [];
+  var response = await http.get(Uri.parse("https://raw.githubusercontent.com/PiotrZPL/CherishOS-OTA/tiramisu/devices.json"));
+  if (response.statusCode == 200) {
+    stdout.write("OK\n");
+    YamlList ydoc = loadYaml(response.body);
+    for (YamlMap device in ydoc) {
+      String readVendor = device["brand"];
+      String readCodename = device["codename"];
+      String codename = readCodename;
+      if (codename == "Mojito" || codename == "Sweet") {
+        codename = codename.toLowerCase();
+      }
+      else if (codename == "mi8937") {
+        codename = "Mi8937";
+      }
+      String extendedCodename = extendedCodenameCreator(readCodename: codename, readVendor: readVendor);
+      stdout.write("$extendedCodename\n");
+      YamlMap version = device["supported_versions"][0];
+      String androidVersion = androidVersionFromCherishOSVersion(cherishOSVersion: version["version_code"].toString().toLowerCase());
+      stdout.write("$androidVersion\n");
+
+      if (isSupported(extendedCodename: extendedCodename)) {
+        numberOfCovered += 1;
+        addToSupport(
+          androidVersion: androidVersion,
+          extendedCodename: extendedCodename,
+          romName: "CherishOS",
+          romState: "Official",
+          romSupport: true,
+          romWebpage: "https://cherishos.com/",
+          deviceWebpage: "https://downloads.cherishos.com/"
+        );
+      }
+      else {
+        numberOfNotCovered += 1;
+        listOfNotCovered += [extendedCodename];
+      }
+    }
+    
+    stdout.write("Covered: $numberOfCovered\n");
+    stdout.write("Not covered: $numberOfNotCovered\n");
+    for (var deviceNotCovered in listOfNotCovered) {
+      stdout.write("$deviceNotCovered\n");
+    }
+  }
+}
+
+String androidVersionFromCherishOSVersion({required String cherishOSVersion}) {
+  if (cherishOSVersion == "tiramisu") {
+    return "13";
+  }
+  else if (cherishOSVersion == "twelve") {
+    return "12";
+  }
+  else if (cherishOSVersion == "eleven") {
+    return "11";
+  }
+  else {
+    throw Exception("This CherishOS version is not supported yet");
+  }
+}
