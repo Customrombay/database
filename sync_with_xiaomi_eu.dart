0016dedc6cd21e0f1c2f09c877d08a1c0b036250
PiotrZPL <zpl20052020@protonmail.ch>
2023-12-13 12:52:46 +0100

    Added support for syncing with xiaomi.eu HyperOS

--- sync_with_xiaomi_eu.dart
+++ sync_with_xiaomi_eu.dart
@@ -8,6 +8,7 @@ void main() {
   List<String> listOfNotCovered = [];
   List<String> listOfCovered = [];
   List<Map> listOfVersions = [
+    {"15" : "https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/files/xiaomi.eu/HyperOS-WEEKLY-RELEASES/"},
     {"14" : "https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/files/xiaomi.eu/MIUI-STABLE-RELEASES/MIUIv14/"},
     {"13" : "https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/files/xiaomi.eu/MIUI-STABLE-RELEASES/MIUIv13/"},
     {"12" : "https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/files/xiaomi.eu/MIUI-STABLE-RELEASES/MIUIv12/"},
@@ -15,7 +16,13 @@ void main() {
     {"10" : "https://xiaomi.eu/community/"}
   ];
   for (Map version in listOfVersions) {
-    File resourceFile = File("filesFromXiaomiEU/xiaomi.eu${version.keys.toList()[0]}.txt");
+    File resourceFile;
+    if (version.keys.toList()[0] == "15") {
+      resourceFile = File("filesFromXiaomiEU/xiaomi.eu.h1.txt");
+    }
+    else {
+      resourceFile = File("filesFromXiaomiEU/xiaomi.eu${version.keys.toList()[0]}.txt");
+    }
     String resourceFileContent = resourceFile.readAsStringSync();
     for (String row in resourceFileContent.split("\n")) {
       if (row != "") {
@@ -31,7 +38,7 @@ void main() {
               romName: "xiaomi.eu",
               romState: "Official",
               romSupport: true,
-              romNotes: "MIUI ${version.keys.toList()[0]}",
+              romNotes: version.keys.toList()[0] == "15" ? "HyperOS 1.0" : "MIUI ${version.keys.toList()[0]}",
               romWebpage: "https://xiaomi.eu/community/",
               deviceWebpage: version.values.toList()[0]
             );
