63278d0a902c10a07f0b1928a34514dc925c5a15
PiotrZPL <zpl20052020@protonmail.ch>
2024-04-29 16:33:55 +0200

    Rework /e/OS syncing support

--- sync_with_e_os.dart
+++ sync_with_e_os.dart
@@ -1,177 +1,126 @@
-// Legacy script, needs rewriting to use the addToSupport() and extendedCodenameCreator() functions
-
 import 'dart:io';
+
 import 'package:yaml/yaml.dart';
-import 'package:yaml_writer/yaml_writer.dart';
-import 'tools/codename_correction.dart';
-import 'tools/android_version_from_letter.dart';
+
+import 'tools/add_to_support.dart';
+import 'tools/extended_codename_creator.dart';
+import 'tools/is_supported.dart';
 
 void main() async {
-  var yamlWriter = YAMLWriter();
+  stdout.write("Cloning https://gitlab.e.foundation/e/documentation/user.git...\n");
   int numberOfCovered = 0;
   int numberOfNotCovered = 0;
-  int numberOfProblematic = 0;
   List<String> listOfNotCovered = [];
-  List listOfProblems = [];
-  for (var file in await Directory("filesFromeOS").list().toList()) {
-    String content = await File(file.path).readAsString();
-    stdout.write(file.path + "\n");
-    var ydoc;
-    try {
-      ydoc = loadYaml(content);
-    } on YamlException catch (exception) {
-      // ydoc = loadYaml(content);
-      numberOfProblematic += 1;
-      listOfProblems += [file.path];
-      continue;
-    }
-    var vendor = ydoc["vendor"];
-    var name = ydoc["name"];
-    var readCodename = ydoc["codename"];
-    var build_version_stable = ydoc["build_version_stable"];
-
-    if (build_version_stable == "Pie" || build_version_stable == "pie") {
-      build_version_stable = "P";
-    }
-    else if (build_version_stable == "Oreo" || build_version_stable == "oreo") {
-      build_version_stable = "O";
-    }
-
-    var build_version_dev = ydoc["build_version_dev"];
-
-    if (build_version_dev == "Nougat") {
-      build_version_dev = "N";
-    }
-    else if (build_version_dev == "Pie" || build_version_dev == "pie") {
-      build_version_dev = "P";
-    }
-    else if (build_version_dev == "Oreo" || build_version_dev == "oreo") {
-      build_version_dev = "O";
-    }
-
-    var eOSVersion;
-
-    if (build_version_stable != null) {
-      eOSVersion = build_version_stable;
-    }
-    else {
-      eOSVersion = build_version_dev;
-    }
-
-    String codename = codenameCorrection(readCodename, vendor);
-
-    // var androidVersion = "";
-    var lineageOSversion = ydoc["current_branch"];
-    // var image = ydoc["image"];
-    // var maintainers = ydoc["maintainers"];
-    var state = "";
-    var phoneWebpage = "https://doc.e.foundation/devices/$readCodename";
-
-    var androidVersion = androidVersionFromLetter(eOSVersion);
-
-    // androidVersion = androidVersionFromLineageOSVersion(lineageOSversion.toString());
-    // try {
-    //   if (maintainers.length > 0) {
-    //     state = "Official";
-    //   }
-    //   else {
-    //     state = "Discontinued";
-    //   }
-    // } on NoSuchMethodError catch (e) {
-    //   state = "Discontinued";
-    // }
-
-    var legacy = ydoc["legacy"];
-    if (legacy == null) {
-      state = "Official";
-    }
-    else {
-      if (legacy == "yes") {
-        state = "Discontinued";
-      }
-      else {
-        throw Exception();
-      }
-    }
-
-    //stdout.write(vendor + "\n");
-
-    bool thisCovered = false;
-    File thisFile = File("database/phone_data/${vendor.toString().toLowerCase()}-$codename.yaml");
-
-    if (await thisFile.exists()) {
-      stdout.write("${vendor.toString().toLowerCase()}-$codename \n");
-      numberOfCovered += 1;
-      thisCovered = true;
-      //stdout.write(numberOfCovered.toString() + "\n");
-    }
-    else {
-      numberOfNotCovered += 1;
-      listOfNotCovered += ["$vendor-$codename"];
-    }
-
-    if (thisCovered) {
-      // File thisFile = File("database/phone_data/${vendor.toString().toLowerCase()}-$codename.yaml");
-      String thisFileContent = await thisFile.readAsString();
-      var thisFileyaml = loadYaml(thisFileContent);
-      List newList = [];
-      bool alreadySupported = false;
-      for (var thisRom in thisFileyaml["roms"]) {
-        String thisRomName = thisRom["rom-name"];
-        if (thisRomName == "/e/OS") {
-          alreadySupported = true;
-          newList += [
-            {
-              "rom-name": "/e/OS",
-              "rom-support": true,
-              "rom-state": state,
-              "android-version": androidVersion,
-              "rom-webpage": "https://e.foundation/e-os/",
-              "phone-webpage": phoneWebpage
+  // List<String> listOfCovered = [];
+  Directory cacheDir = Directory(".cache/eOSSync");
+  if (cacheDir.existsSync()) {
+    cacheDir.deleteSync(recursive: true);
+  }
+  cacheDir.createSync(recursive: true);
+  var process = await Process.start("git", ["clone", "https://gitlab.e.foundation/e/documentation/user.git", cacheDir.path], mode: ProcessStartMode.inheritStdio);
+  int exitCode = await process.exitCode;
+  if (exitCode == 0) {
+    stdout.write("OK\n");
+    for (FileSystemEntity entry in Directory("${cacheDir.path}/htdocs/_data/devices").listSync().toList()) {
+      stdout.write("${entry.path}\n");
+      if (entry is File) {
+        String content = await entry.readAsString();
+        List<String> listOfLines = content.split("\n");
+        List<String> listOfParsedLines = [];
+        for (String line in listOfLines) {
+          String firstPart = line.split(":").first;
+          bool repeats = false;
+          for (String parsedLine in listOfParsedLines) {
+            String parsedFirstPart = parsedLine.split(":").first;
+            if (firstPart == parsedFirstPart) {
+              repeats = true;
             }
-          ];
+          }
+          if (!repeats) {
+            listOfParsedLines += [line];
+          }
+        }
+        content = listOfParsedLines.join("\n");
+        YamlMap yamlMap = loadYaml(content);
+        String devVersion = yamlMap["build_version_dev"] ?? "";
+        String? stableVersion = yamlMap["build_version_stable"];
+        String? isLegacy = yamlMap["legacy"];
+        String androidVersion;
+        String romStatus;
+        if (stableVersion != null) {
+          androidVersion = androidVersionFromeOSVersion(stableVersion);
+          romStatus = "Official";
         }
         else {
-          newList += [thisRom];
+          androidVersion = androidVersionFromeOSVersion(devVersion);
+          romStatus = "Community";
+        }
+        if (isLegacy != null) {
+          if (isLegacy == "yes") {
+            romStatus = "Discontinued";
+          }
+          else {
+            throw Exception();
+          }
+        }
+        print(androidVersion);
+        String codename = yamlMap["codename"];
+        String manufacturer = yamlMap["vendor_short"] ?? (yamlMap["vendor"] as String).toLowerCase();
+        String extendedCodename = extendedCodenameCreator(readCodename: codename, readVendor: manufacturer);
+        if (isSupported(extendedCodename: extendedCodename)) {
+          // listOfCovered += [extendedCodename];
+          numberOfCovered += 1;
+          addToSupport(
+            androidVersion: androidVersion,
+            extendedCodename: extendedCodename,
+            romName: "/e/OS",
+            romState: romStatus,
+            romSupport: true,
+            romWebpage: "https://e.foundation/e-os/",
+            deviceWebpage: "https://doc.e.foundation/devices/$codename",
+          );
+        }
+        else {
+          listOfNotCovered += [extendedCodename];
+          numberOfNotCovered += 1;
         }
       }
+    }
 
-      if (!alreadySupported) {
-        newList += [
-          {
-            "rom-name": "/e/OS",
-            "rom-support": true,
-            "rom-state": state,
-            "android-version": androidVersion,
-            "rom-webpage": "https://e.foundation/e-os/",
-            "phone-webpage": phoneWebpage
-          }
-        ];
-      }
-
-      Map newMap = {
-        "device-name" : thisFileyaml["device-name"],
-        "device-vendor": thisFileyaml["device-vendor"],
-        "device-model-name": thisFileyaml["device-model-name"],
-        "device-description": thisFileyaml["device-description"],
-        "specs": thisFileyaml["specs"],
-        "roms": newList,
-        "recoveries": thisFileyaml["recoveries"],
-        "linux": thisFileyaml["linux"],
-      };
-
-      // File newFile = File("newfiles/${vendor.toString().toLowerCase()}-$codename.yaml");
-      await thisFile.writeAsString(yamlWriter.write(newMap));
+    stdout.write("Covered: $numberOfCovered\n");
+    stdout.write("Not covered: $numberOfNotCovered\n");
+    for (String device in listOfNotCovered) {
+      stdout.write("$device\n");
     }
   }
-  stdout.write("Covered: $numberOfCovered\n");
-  stdout.write("Not covered: $numberOfNotCovered\n");
-  stdout.write("Problematic: $numberOfProblematic\n");
-  for (var device in listOfNotCovered) {
-    stdout.write("$device\n");
-  }
-  for (var ok in listOfProblems) {
-    stdout.write("$ok\n");
-  }
+}
 
+String androidVersionFromeOSVersion(String eOSVersion) {
+  if (eOSVersion == "Q") {
+    return "10";
+  }
+  else if (eOSVersion == "R") {
+    return "11";
+  }
+  else if (eOSVersion == "S") {
+    return "12";
+  }
+  else if (eOSVersion == "T") {
+    return "13";
+  }
+  else if (eOSVersion == "U") {
+    return "14";
+  }
+  else if (eOSVersion == "Oreo") {
+    return "8";
+  }
+  else if (eOSVersion == "Pie") {
+    return "9";
+  }
+  else if (eOSVersion == "Nougat") {
+    return "7";
+  }
+  else {
+    throw Exception("This version of eOS ($eOSVersion) is not supported yet!");
+  }
 }
